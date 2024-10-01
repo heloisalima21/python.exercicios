8e5c656ad7e390ae73c6e9536684bdd15de29630
heloisalima21 <heloisalimassa@gmail.com>
2024-10-01 19:57:52 +0000

    exercicio dados de familia

--- exercicio.py
+++ exercicio.py
@@ -1,3 +1,13 @@
+"""
+Informe o número da turma: 
+Turma - 93313
+
+Nome completo dos componentes.
+1 - Heloisa Lima de Oliveira
+2 - júlia Vitória dos Santos Azevedo Jesus
+
+"""
+
 import os
 os.system("cls || clear")
 
